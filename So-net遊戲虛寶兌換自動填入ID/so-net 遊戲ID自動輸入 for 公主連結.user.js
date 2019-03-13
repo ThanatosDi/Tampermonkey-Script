479f191fcdfe0abb8c029b3eec9a898e6ed22dab
古丁丁 <s14113103@stu.edu.tw>
2019-03-13 18:06:29 +0800

    add server select by S.Dot

--- So-net遊戲虛寶兌換自動填入ID/so-net 遊戲ID自動輸入 for 公主連結.user.js
+++ So-net遊戲虛寶兌換自動填入ID/so-net 遊戲ID自動輸入 for 公主連結.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         so-net 遊戲ID自動輸入 for 公主連結
 // @namespace    https://github.com/Kutinging/svp.so-ne
-// @version      1.1
+// @version      1.2
 // @description  so-net 遊戲ID自動輸入 for 公主連結
 // @author       ThanatosDi , S.Dot
-// @match        http://www.svp.so-net.tw/
+// @match        http://www.svp.so-net.tw/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-   document.querySelector("select#game_no").value = "SON009"
-   document.querySelector("input#user_id").value="(你的公主連結帳號ID)"
+   document.querySelector("select#game_no").value = "SON009" //遊戲代號 公主連結
+   document.querySelector("select#game_no").dispatchEvent(new Event('change'))
+   document.querySelector("input#user_id").value="" //遊戲 UID
+   document.querySelector("select#server_channel_list").value="1" //伺服器 1=美食殿堂 2=真布真布王國
    document.querySelector("input#user_id").focus();
    document.querySelector("input#vp_no").focus();
    var form = document.getElementsByTagName('form')[0];
