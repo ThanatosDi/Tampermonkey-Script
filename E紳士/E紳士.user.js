2de1fc1649cd88d07202a73ddd65b87375e3d5d8
古丁丁 <s14113103@stu.edu.tw>
2019-08-11 16:21:32 +0800

    修正在裏站時無法顯示第二頁以後等錯誤
     * 修正在裏站時無法顯示第二頁以後的錯誤
     * 從新建構抓頁面方法

--- E紳士/E紳士.user.js
+++ E紳士/E紳士.user.js
@@ -12,23 +12,31 @@
 // ==/UserScript==
 var fetch_urls = []
 var Imgurls = []
+var page
 const reply = document.getElementById('cdiv')
 const div = document.createElement('div')
 setAttributes(div, {'id':'images', 'style': 'margin:10px auto;width:720px;max-width:100%;'})
-var pages = document.querySelector('#gdd > table > tbody > tr:nth-child(6) > td.gdt2').innerText.match(/\d+/)[0]
-console.log('total ' + pages + ' imgs')
+
+if(location.hostname=='e-hentai.org'){
+    console.log('e-hentai.org')
+    page = $('body > div:nth-child(9) > table > tbody > tr > td').length-2
+}else if(location.hostname=='exhentai.org'){
+    console.log('exhentai.org')
+    page = $('body > div:nth-child(6) > table > tbody > tr > td').length-2
+}
+console.log('page = '+page)
 
 function main(){
-	GetAllImgs(0)
+    GetAllImgs(0)
 }
 
 function GetAllImgs(index){
-	console.log(index)
-	var target = window.location.href + `?p=${index}`
-	fetch(target)
-	.then(data=>{return data.text()})
-	.then(htmlbody=>{
-		var container = document.implementation.createHTMLDocument().documentElement;
+    //console.log('now page = '+index)
+    var target = window.location.href + `?p=${index}`
+    fetch(target)
+        .then(data=>{return data.text()})
+        .then(htmlbody=>{
+        var container = document.implementation.createHTMLDocument().documentElement;
         container.innerHTML = htmlbody
         if(container.querySelectorAll('.gdtm a').length>0){
             container.querySelectorAll('.gdtm a').forEach(ele=>{fetch_urls.push(ele.href)})
@@ -37,41 +45,41 @@ function GetAllImgs(index){
         }
         //container.querySelectorAll('.gdtm a').forEach(ele=>{fetch_urls.push(ele.href)})
         //console.log(container.querySelectorAll('.gdtm a'))
-	})
-	.then(()=>{
-		console.log(fetch_urls)
-		console.log(index,pages/40)
-		if(index < pages/40-1){
-			index+=1
-			GetAllImgs(index)
-		}
-		else
-			fetchImgUrl(0)
-	})
+    })
+        .then(()=>{
+        //console.log(fetch_urls)
+        if(index < page-1){
+            index+=1
+            GetAllImgs(index)
+        }
+        else
+            fetchImgUrl(0)
+    })
 }
 
 
 function fetchImgUrl(index){
-	fetch(fetch_urls[index])
-	.then(data=>{return data.text()})
-	.then(htmlbody=>{
-			    var container = document.implementation.createHTMLDocument().documentElement;
-                const img = document.createElement('img')
-		        container.innerHTML = htmlbody
-		        var Imgurl = container.querySelector('#img').src;
-		        console.log(Imgurl)
-		        Imgurls.push(Imgurl)
-                setAttributes(img, {'id': 'img_'+(index+1), 'style': 'display:block;margin:4px auto;max-width:100%;min-width:100px;min-height:100px;background:#000;', 'src': Imgurl})
-                div.appendChild(img)
-                reply.parentNode.insertBefore(div, reply)
-	})
-	.then(()=>{
-		if(index != fetch_urls.length-1){
-			console.log(index,fetch_urls.length-1)
-			index += 1
-			fetchImgUrl(index)
-		}
-	})
+    fetch(fetch_urls[index])
+        .then(data=>{return data.text()})
+        .then(htmlbody=>{
+        console.log('image loading...('+(index+1)+'/'+fetch_urls.length+')')
+        var container = document.implementation.createHTMLDocument().documentElement;
+        const img = document.createElement('img')
+        container.innerHTML = htmlbody
+        var Imgurl = container.querySelector('#img').src;
+        //console.log(Imgurl)
+        Imgurls.push(Imgurl)
+        setAttributes(img, {'id': 'img_'+(index+1), 'style': 'display:block;margin:4px auto;max-width:100%;min-width:100px;min-height:100px;background:#000;', 'src': Imgurl})
+        div.appendChild(img)
+        reply.parentNode.insertBefore(div, reply)
+    })
+        .then(()=>{
+        if(index != fetch_urls.length-1){
+            //console.log('('+index+'/'+fetch_urls.length+')')
+            index += 1
+            fetchImgUrl(index)
+        }
+    })
 }
 function setAttributes(el, attrs) {
     for(var key in attrs) {
